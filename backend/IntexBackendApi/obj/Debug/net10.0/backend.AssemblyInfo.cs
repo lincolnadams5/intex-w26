3788356999b2c7f74b8ef2ed832bfdee16cf1bb2
Cassequel <apierceswan@gmail.com>
2026-04-06 11:19:54 -0600

    aiden added build files

--- backend/IntexBackendApi/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/IntexBackendApi/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a48b572e87c4ec9abbf1f428b0f635deacf3190a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d44ecf085510e9a8c7e20473d0b627ab4fb3fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
